103a2ba6c8509ac2506c61cc76e3b72840c38e13
Zachary Ciatteo <zciatteo96@gmail.com>
2023-01-02 20:16:04 -0500

    update add comment

--- components/AddComment.js
+++ components/AddComment.js
@@ -4,16 +4,38 @@ import Avatar from './Avatar';
 export default function AddComment() {
   return (
     <AddCommentStyled>
-      <Avatar size="xsmall" avatar={'./images/avatars/image-juliusomo.png'} />
-      <textarea name="" id="" cols="30" rows="10"></textarea>
-      <button type="submit">Submit</button>
+      <Avatar size="medium" avatar={'./images/avatars/image-juliusomo.png'} />
+      <CommentTextareaStyled
+        name="comment"
+        id="comment"
+        rows="5"
+        placeholder="Add a Comment..."
+      ></CommentTextareaStyled>
+      <CommentSubmitStyled type="submit">Submit</CommentSubmitStyled>
     </AddCommentStyled>
   );
 }
 
 const AddCommentStyled = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 1rem;
   padding: 1.2rem;
   border-radius: 0.5rem;
   background-color: white;
   width: 100%;
+`;
+
+const CommentSubmitStyled = styled.button`
+  background-color: hsl(238, 40%, 52%);
+  color: #fff;
+  border: 0;
+  padding: 1rem;
+  border-radius: 0.5rem;
+`;
+
+const CommentTextareaStyled = styled.textarea`
+  flex: 1;
+  border-radius: 0.5rem;
+  padding: 1rem;
 `;
